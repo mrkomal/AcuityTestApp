f9e4d26a0114a82491b7f20400f3dc44e4dcec92
mrkomal <gry2@vp.pl>
2020-11-11 21:07:58 +0100

    Adding view model TAG

--- app/src/main/java/com/example/acuitytestapp/viewmodel/ScoreViewModel.kt
+++ app/src/main/java/com/example/acuitytestapp/viewmodel/ScoreViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.ViewModel
 
 class ScoreViewModel : ViewModel() {
 
+    //tag for logging
+    val TAG = "ScoreViewModel"
+
     //variable that holds the number of already displayed letters
-    private var numOfDisLetters : Int = 0
+    var numOfDisLetters : Int = 0
 
     //variable that keeps track of the user's correct answers
-    private var numOfCorrAnswers : Int = 0
+    var numOfCorrAnswers : Int = 0
 
 }
